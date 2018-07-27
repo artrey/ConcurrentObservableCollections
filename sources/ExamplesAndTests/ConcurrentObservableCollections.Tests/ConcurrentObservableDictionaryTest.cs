b6faa900afa855ae615e60ef3bb693ab8a1f0728
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2018-07-27 14:43:36 +0300

    Added tests

---
+++ sources/ExamplesAndTests/ConcurrentObservableCollections.Tests/ConcurrentObservableDictionaryTest.cs
@@ -0,0 +1,64 @@
+﻿using ConcurrentObservableCollections.ConcurrentObservableDictionary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConcurrentObservableCollections.Tests
+{
+    [TestClass]
+    public class ConcurrentObservableDictionaryTest
+    {
+        [TestMethod]
+        public void TestEvents()
+        {
+            var data = new ConcurrentObservableDictionary<string, double>();
+            var updated = false;
+            data.CollectionChanged += (s, e) => { updated = true; };
+
+            data.AddOrUpdate("test", 1.0);
+
+            Assert.IsTrue(updated);
+        }
+
+        [TestMethod]
+        public void TestAddPartialEvents()
+        {
+            var data = new ConcurrentObservableDictionary<string, double>();
+            var obs = new SimpleObserver();
+            data.AddPartialObserver(obs, "test", "test2");
+
+            data.AddOrUpdate("test", 1.0);
+            Assert.AreEqual(1.0, obs.LastValue, "Error in test key");
+
+            data.AddOrUpdate("test2", 10.0);
+            Assert.AreEqual(10.0, obs.LastValue, "Error in test2 key");
+        }
+
+        [TestMethod]
+        public void TestRemovePartialEvents()
+        {
+            var data = new ConcurrentObservableDictionary<string, double>();
+            var obs = new SimpleObserver();
+            data.AddPartialObserver(obs, "test", "test2", "test3");
+
+            data.AddOrUpdate("test", 1.0);
+            Assert.AreEqual(1.0, obs.LastValue, "Error in test key");
+
+            Assert.IsTrue(data.RemovePartialObserver(obs, "test"), "remove <obs, test>");
+
+            data.AddOrUpdate("test", 10.0);
+            Assert.AreEqual(1.0, obs.LastValue, "Error in test key after remove <obs, test>");
+
+            Assert.IsTrue(data.RemovePartialObserver("test3"), "remove test3 key");
+
+            data.AddOrUpdate("test3", 30.0);
+            Assert.AreEqual(1.0, obs.LastValue, "Error in test3 key after remove test3 key");
+
+            data.AddOrUpdate("test2", 2.0);
+            Assert.AreEqual(2.0, obs.LastValue, "Error in test2 key");
+
+            Assert.IsTrue(data.RemovePartialObserver(obs), "remove obs");
+
+            data.AddOrUpdate("test2", 20.0);
+            Assert.AreEqual(2.0, obs.LastValue, "Error in test2 key after remove obs");
+        }
+    }
+}
---
+++ sources/ExamplesAndTests/ConcurrentObservableCollections.Tests/SimpleObserver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConcurrentObservableCollections.ConcurrentObservableDictionary;
+
+namespace ConcurrentObservableCollections.Tests
+{
+    public class SimpleObserver : IDictionaryObserver<string, double>
+    {
+        public double LastValue { get; private set; }
+
+        public void OnEventOccur(DictionaryChangedEventArgs<string, double> args)
+        {
+            LastValue = args.NewValue;
+        }
+    }
+}
